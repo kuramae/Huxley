a32f8da37c0290c98b241d5c6261c9be23501fa6
Daniel <daniel.sykes@imperial.ac.uk>
2012-12-04 23:44:33 +0000

    Fixed location

--- androidsrc/src/uk/hux/Location.java
+++ androidsrc/src/uk/hux/Location.java
@@ -8,8 +8,7 @@ public class Location extends UserStatus
   
   public Location(long user, long time, double lat, double lon, double accuracy)
   {
-    this.userID = user;
-    this.timestamp = time;
+    super(user, time);
     this.latitude = lat;
     this.longitude = lon;
     this.accuracy = accuracy;
--- androidsrc/src/uk/hux/UserStatus.java
+++ androidsrc/src/uk/hux/UserStatus.java
@@ -4,4 +4,10 @@ public abstract class UserStatus
 {
   public final long userID;
   public final long timestamp; //unix
+  
+  protected UserStatus(long uid, long time)
+  {
+    this.userId = uid;
+    this.timestamp = time;
+  }
 }
