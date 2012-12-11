d23cf11fff7bb05784798dee7bc754a2576d7bd7
Domenico <do.corapi@gmail.com>
2012-12-11 20:14:35 +0000

    Google places stuff

--- androidsrc/src/uk/hux/UserStatus.java
+++ androidsrc/src/uk/hux/UserStatus.java
@@ -2,6 +2,12 @@ package uk.hux;
 
 public abstract class UserStatus
 {
-  public final long userID;
-  public final long timestamp; //unix
+	public final long userID;
+	public final long timestamp; //unix
+	
+	protected UserStatus(long userID, long timestamp)
+	{
+		this.userID = userID;
+		this.timestamp = timestamp;
+	}
 }
