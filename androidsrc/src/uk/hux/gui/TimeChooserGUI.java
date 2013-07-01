666ff738cce8db005578e06ef0c7db5bfadc0468
Daniel <daniel.sykes@imperial.ac.uk>
2013-07-01 13:44:10 +0100

    Improved TimeChooser layout

--- androidsrc/src/uk/hux/gui/TimeChooserGUI.java
+++ androidsrc/src/uk/hux/gui/TimeChooserGUI.java
@@ -3,7 +3,7 @@ package uk.hux.gui;
 import uk.hux.service.*;
 import uk.hux.*;
 import android.app.*;
-import android.os.Bundle;
+import android.os.*;
 import android.location.*;
 import android.widget.*;
 import android.content.*;
@@ -15,43 +15,45 @@ public class TimeChooserGUI extends Activity
   private DatePicker date;
   private TimePicker time;
   private Button whereButton;
+  private TextView text;
   
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.time);
     
-    final TextView text = new TextView(this);
+    date = (DatePicker) findViewById(R.id.date);
+    time = (TimePicker) findViewById(R.id.time);
+    whereButton = (Button) findViewById(R.id.where);
+    text = (TextView) findViewById(R.id.text);
     
-    date = new DatePicker(this);
     date.setMaxDate(System.currentTimeMillis());
-    
-    time = new TimePicker(this);
-    
-    whereButton = new Button(this);
-    whereButton.setText("So where the fuck was I?");
+
     whereButton.setOnClickListener(new View.OnClickListener()
       {
         public void onClick(View v)
         {
-          GregorianCalendar cal = new GregorianCalendar(date.getYear(), date.getMonth(), date.getDayOfMonth(), time.getCurrentHour(), time.getCurrentMinute());
-          List<Place> places = HuxleyCore.core.log.whereWereYou(cal.getTimeInMillis());
-          if (places != null)
-          {
-            for (Place p : places)
-              text.append(p.name+" ?\n");
-          }
-          else
-            text.append("No places returned\n");
+          whereButton.setEnabled(false);
+          Handler handler = new Handler();
+          handler.post(new Runnable()
+            {
+              public void run()
+              {
+                GregorianCalendar cal = new GregorianCalendar(date.getYear(), date.getMonth(), date.getDayOfMonth(), time.getCurrentHour(), time.getCurrentMinute());
+                List<Place> places = HuxleyCore.core.log.whereWereYou(cal.getTimeInMillis());
+                text.setText("");
+                if (places != null)
+                {
+                  for (Place p : places)
+                    text.append(p.name+" ?\n");
+                }
+                else
+                  text.append("No places returned\n");
+                whereButton.setEnabled(true);
+              }
+            });
         }
       });
-      
-    TableLayout layout = new TableLayout(this);
-    layout.addView(date);
-    layout.addView(time);
-    layout.addView(whereButton);
-    layout.addView(text);
-    
-    setContentView(layout);
   }
 }
