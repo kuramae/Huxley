63a625ef10d1ca3f48d80fc0f99bb9813d8f0fda
antolugo <corapi@live.it>
2014-01-02 23:15:42 +0100

    add soml

---
+++ soml/soml/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  soml
+//
+//  Created by Anto on 30/12/13.
+//  Copyright (c) 2013 Anto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ soml/soml/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  soml
+//
+//  Created by Anto on 30/12/13.
+//  Copyright (c) 2013 Anto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
