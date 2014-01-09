9faa104927e01a240bcd7c7c6512f5b66b2a8b0c
Daniel <daniel.sykes@imperial.ac.uk>
2014-01-09 16:34:14 +0000

    Skeleton for server response to where requests

---
+++ huxley-server/src/main/java/com/huxley/WhereWereYou.java
@@ -0,0 +1,19 @@
+package com.huxley;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import org.json.*;
+
+@Path("where")
+public class WhereWereYou
+{
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String whereWereYou(@QueryParam("userid") String userID, @QueryParam("timestamp") long timestamp)
+  {
+    //look in DB for userID
+    //interpolate for timestamp
+    //ask google for list of potentials
+    return (new JSONArray()).toString(); //empty
+  }
+}
